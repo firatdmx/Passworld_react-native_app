f9f36d94984637031ee00fca26d6adef6ea33ab1
firatdmx <powerprojectionx@gmail.com>
2023-09-15 23:30:08 +0300

    app.js removed comments

--- App.js
+++ App.js
@@ -1,18 +1,8 @@
 import "react-native-gesture-handler"
 import React from "react";
-
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import { createDrawerNavigator } from '@react-navigation/drawer';
-// import DrawerLeft from "./src/components/DrawerLeft";
-
-// const Drawer = createDrawerNavigator();
-
-
 import  {store} from "./src/app/store"
 import { Provider } from 'react-redux'
 import Router from "./src/Router";
-
-// const Stack = createNativeStackNavigator();
 
 
 const App = () => {
